f700af1c6107dd55f99733394d94192ab6620332
dylangabrielbaezolveira-arch <dylangabrielbaezolveira@gmail.com>
2025-12-26 17:34:07 -0300

    Update CarData.cs

--- Assets/ScriptableObjects/Cars/CarData.cs
+++ Assets/ScriptableObjects/Cars/CarData.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-namespace SwyPhexLeague.ScriptableObjects
+namespace SwyPhexLeague.ScriptableObjects.Cars
 {
-    [CreateAssetMenu(fileName = "NewCarData", menuName = "SwyPhex/Car Data")]
+    [CreateAssetMenu(fileName = "CarData", menuName = "SwyPhex/Car Data")]
     public class CarData : ScriptableObject
     {
-        [Header("Car Identity")]
-        public string carId;
-        public string carName;
-        public string description;
+        [Header("Identification")]
+        public string carId = "PHEX-01";
+        public string displayName = "PHEX-01";
+        public string description = "Balanced car with Pulse Dash ability";
         public Sprite icon;
-        public GameObject carPrefab;
+        public GameObject prefab;
+        
+        [Header("Unlock Requirements")]
         public int unlockLevel = 1;
-        public int cost = 500;
+        public int cost = 0;
+        public bool isPremium = false;
         
         [Header("Base Stats")]
         [Range(0.5f, 2f)] public float speedMultiplier = 1f;
@@ -20,44 +23,79 @@ namespace SwyPhexLeague.ScriptableObjects
         [Range(0.5f, 2f)] public float turnMultiplier = 1f;
         [Range(0.5f, 2f)] public float jumpMultiplier = 1f;
         [Range(0.5f, 2f)] public float boostMultiplier = 1f;
+        [Range(0.5f, 2f)] public float weightMultiplier = 1f;
         
         [Header("Ability")]
-        public string abilityName;
+        public string abilityName = "PulseDash";
         public float abilityCooldown = 8f;
         public float abilityBoostCost = 5f;
         public Sprite abilityIcon;
-        public GameObject abilityEffect;
+        public GameObject abilityPrefab;
         
         [Header("Visual Customization")]
         public Material[] availableMaterials;
         public Color[] availableColors;
         public GameObject[] availableDecals;
+        public ParticleSystem[] availableTrails;
         
         [Header("Audio")]
         public AudioClip engineSound;
         public AudioClip boostSound;
         public AudioClip abilitySound;
+        public AudioClip collisionSound;
         
-        public float GetStatValue(CarStat stat)
+        [Header("UI")]
+        public Color uiColor = Color.white;
+        public Sprite statChart;
+        
+        public float GetStatValue(StatType stat)
         {
-            switch (stat)
+            return stat switch
             {
-                case CarStat.Speed: return 15f * speedMultiplier;
-                case CarStat.Acceleration: return 35f * accelerationMultiplier;
-                case CarStat.Turn: return 3.5f * turnMultiplier;
-                case CarStat.Jump: return 12f * jumpMultiplier;
-                case CarStat.Boost: return 12f * boostMultiplier;
-                default: return 1f;
-            }
+                StatType.Speed => 15f * speedMultiplier,
+                StatType.Acceleration => 35f * accelerationMultiplier,
+                StatType.Turn => 3.5f * turnMultiplier,
+                StatType.Jump => 12f * jumpMultiplier,
+                StatType.Boost => 12f * boostMultiplier,
+                StatType.Weight => 1.2f * weightMultiplier,
+                _ => 1f
+            };
         }
         
-        public enum CarStat
+        public float[] GetAllStats()
+        {
+            return new float[]
+            {
+                speedMultiplier,
+                accelerationMultiplier,
+                turnMultiplier,
+                jumpMultiplier,
+                boostMultiplier,
+                weightMultiplier
+            };
+        }
+        
+        public string[] GetStatNames()
+        {
+            return new string[]
+            {
+                "Speed",
+                "Acceleration",
+                "Turn",
+                "Jump",
+                "Boost",
+                "Weight"
+            };
+        }
+        
+        public enum StatType
         {
             Speed,
             Acceleration,
             Turn,
             Jump,
-            Boost
+            Boost,
+            Weight
         }
     }
 }
